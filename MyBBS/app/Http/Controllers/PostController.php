3fae808095c779f35f3d68e4e3a571e035666f1e
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-15 21:27:21 +0900

    08 入力された値を検証しよう

--- MyBBS/app/Http/Controllers/PostController.php
+++ MyBBS/app/Http/Controllers/PostController.php
@@ -28,6 +28,11 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'title' => 'required|min:3',
+            'body' => 'required',
+        ]);
+
         $post = new Post();
         $post->title = $request->title;
         $post->body = $request->body;
