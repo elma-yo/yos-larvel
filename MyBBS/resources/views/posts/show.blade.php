b77551c251d96f0ad1a896b6e67af60a2604f55e
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-16 13:23:32 +0900

    13 編集フォームのViewを作ろう

--- MyBBS/resources/views/posts/show.blade.php
+++ MyBBS/resources/views/posts/show.blade.php
@@ -7,6 +7,9 @@
         &laquo; <a href="{{ route('posts.index') }}">Back</a>
     </div>
 
-    <h1>{{ $post->title }}</h1>
-    <p>{!! nl2br($post->body) !!}</p>
+    <h1>
+        <span>{{ $post->title }}</span>
+        <a href="{{ route('posts.edit', $post) }}">[Edit]</a>
+    </h1>
+    <p>{!! nl2br(e($post->body)) !!}</p>
 </x-layout>
