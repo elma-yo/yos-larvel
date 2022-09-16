2cfca6b5b6ae4c6dfb77681e1db4d86a86dfd328
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-16 15:33:49 +0900

    10 コメントを追加できるようにしよう

--- MyBBS/resources/views/posts/show.blade.php
+++ MyBBS/resources/views/posts/show.blade.php
@@ -20,6 +20,15 @@
 
     <h2>Comments</h2>
     <ul>
+
+        <li>
+            <form method="post" action="{{ route('comments.store', $post) }}" class="comment-form">
+                @csrf
+
+                <input type="text" name="body">
+                <button>Add</button>
+            </form>
+        </li>
         @foreach ($post->comments as $comment)
             <li>
                 {{ $comment->body }}
